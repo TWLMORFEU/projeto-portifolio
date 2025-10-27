2f3ab769c81ec55aa4d348aed40a259f70d0b01b
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-10-27 17:22:53 -0300

    atualização, implementando, responsividade a telas maiores e retomada de scrool suave

--- scripts/script.js
+++ scripts/script.js
@@ -31,3 +31,19 @@ function toggleConteudo(botao) {
   botao.textContent = visivel ? "Ver conteúdo" : "Ocultar conteúdo";
 }
 
+// Scroll suave para links de navegação
+const navlinks = document.querySelectorAll('#menu ul  a.link');
+navlinks.forEach(link => {
+  link.addEventListener(`click`, function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if(target) {
+      const headerHeight = document.querySelector('header').offsetHeight;
+      const targetPosition = target.offsetTop - headerHeight -  20;
+      window.scrollTo({
+        top: targetPosition, 
+        behavior: 'smooth'
+      });
+    }
+  });
+});
